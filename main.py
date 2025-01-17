60ddc8177e99528aa7c575d5c24a0b9503383fb9
kristof.tajti <kristof.tajti@gmail.com>
2025-01-17 20:03:56 +0100

    Vector db implementation + testing started

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import argparse
 from utils import pdf_processor
 from vector_db.vector_db import WeaviateVectorDatabase
 # NOTE - add LLM choice
+
+import weaviate.classes as wvc
+import requests
+import pandas as pd
+
 def parse_args() -> argparse.Namespace:
     """
     Parse the command line arguments for RAG.
@@ -16,8 +21,34 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     args, modifiers = parse_args()
-    vector_db = WeaviateVectorDatabase(host='host.docker.internal',port='8080') 
+    vector_db = WeaviateVectorDatabase(host='host.docker.internal',port='8080')
+    vector_db.delete_collection("test")
+    ## testonly
+    properties=[
+        wvc.config.Property(
+            name="question",
+            data_type=wvc.config.DataType.TEXT,
+        ),
+        wvc.config.Property(
+            name="answer",
+            data_type=wvc.config.DataType.TEXT,
+        ),
+        wvc.config.Property(
+            name="category",
+            data_type=wvc.config.DataType.TEXT,
+        )
+    ]
+    vector_db.create_collection(collection_name="test", properties=properties)
 
+    resp = requests.get(
+        "https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json"
+    )
+    df = pd.DataFrame(resp.json())
+    transformed_data = [{col: row[col] for col in df.columns} for _, row in df.iterrows()]
+
+    vector_db.add_documents("test", transformed_data)
+    response = vector_db.search(collection_name='test', query_vector="animal", search_type='bm25', filters = wvc.query.Filter.by_property("category").equal("Animals"))
+    print(response)
 
 if __name__ == "__main__":
     main()
--- vector_db/vector_db.py
+++ vector_db/vector_db.py
@@ -1,4 +1,8 @@
+import syslog
 import weaviate
+import weaviate.classes as wvc
+
+from tqdm import tqdm
 from typing import List, Dict, Optional
 
 class WeaviateVectorDatabase:
@@ -8,12 +12,22 @@ class WeaviateVectorDatabase:
         """
         self.client = weaviate.connect_to_local(host=host, port=port)
 
-    def create_collection(self, name: str, properties: List[Dict], vectorizer: str = "text2vec-transformers"):
+    def create_collection(self, collection_name: str, properties: List[Dict], vectorizer: str = "text2vec-transformers"):
         """
         Create a collection (class) in Weaviate.
         """
+        existing_collections = self._get_collection_names()
+        existing_collection_names = [collection_name.lower() for collection_name in existing_collections.keys()]
+        if str.lower(collection_name) in existing_collection_names:
+            raise ValueError(f"Collection '{collection_name}' already exists. Please choose a different name.")
+
         if vectorizer == "text2vec-transformers":
-            ...
+            _ = self.client.collections.create(
+                name=collection_name,
+                vectorizer_config=[wvc.config.Configure.NamedVectors.text2vec_transformers(name="vectorzier")],
+                properties=properties
+            )
+            syslog.syslog(f"Collection {collection_name} created.")
         else:
             NotImplementedError("Vectorizer not implemented.")
 
@@ -21,25 +35,50 @@ class WeaviateVectorDatabase:
         """
         Delete a collection (class) in Weaviate.
         """
-        pass
+        self.client.collections.delete(name)
+        syslog.syslog(f"Collection {name} deleted.")
 
-    def list_collections(self) -> List[str]:
+    def _get_collection_names(self) -> List[str]:
         """
         List all collections (classes) in Weaviate.
         """
-        pass
+        response = self.client.collections.list_all(simple=True)
+        return response
 
     def add_documents(self, collection_name: str, documents: List[Dict], embeddings: Optional[List[List[float]]] = None):
         """
         Add documents with optional embeddings.
         """
-        pass
+        collection = self.client.collections.get(collection_name)
 
-    def search(self, collection_name: str, query_vector: List[float], top_k: int = 5, filters: Optional[Dict] = None) -> List[Dict]:
+        with collection.batch.dynamic() as batch:
+            for i, document in tqdm(enumerate(documents)):
+                if embeddings:
+                    batch.add_object(properties = document, vector=embeddings[i])
+                else:
+                    batch.add_object(properties = document)
+
+        if len(collection.batch.failed_objects) > 0:
+            syslog.syslog(f"Failed to import {len(collection.batch.failed_objects)} objects")
+
+    def search(self, collection_name: str, query_vector: List[float], top_k: int = 5, search_type:str='semantic', filters: Optional[Dict] = None) -> List[Dict]:
         """
         Search the collection using a query vector with optional filters.
         """
-        pass
+        # TODO - should probs check the collection name aswell - make that a separate func as it seems to come in handy
+        existing_collections = self._get_collection_names()
+        existing_collection_names = [collection_name.lower() for collection_name in existing_collections.keys()]
+        if str.lower(collection_name) not in existing_collection_names:
+            raise ValueError(f"Collection '{collection_name}' does not exist!")
+
+        collection = self.client.collections.get(collection_name)
+        if str.lower(search_type) == 'semantic':
+            return collection.query.near_text(query=query_vector, limit=top_k, filters=filters)
+        elif str.lower(search_type) == 'bm25':
+            return collection.query.bm25(query=query_vector, limit=top_k, filters=filters)
+        else:
+            NotImplementedError("Search type not implemented. Try: Semantic or BM25")
+
 
     def delete_documents(self, collection_name: str, document_ids: List[str]):
         """
